e12ef9ed3005e2ea71924b4f62480c8a49be0e25
Phoenix <bootcampgithub@gmail.com>
2024-03-05 12:57:39 +0000

    revise indentation

--- script.js
+++ script.js
@@ -1,101 +1,17 @@
 // Array of special characters to be included in password
-var specialCharacters = [
-  '@',
-  '%',
-  '+',
-  '\\',
-  '/',
-  "'",
-  '!',
-  '#',
-  '$',
-  '^',
-  '?',
-  ':',
-  ',',
-  ')',
-  '(',
-  '}',
-  '{',
-  ']',
-  '[',
-  '~',
-  '-',
-  '_',
-  '.'
-];
+var specialCharacters = ['@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'];
 
 // Array of numeric characters to be included in password
 var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
 // Array of lowercase characters to be included in password
-var lowerCasedCharacters = [
-  'a',
-  'b',
-  'c',
-  'd',
-  'e',
-  'f',
-  'g',
-  'h',
-  'i',
-  'j',
-  'k',
-  'l',
-  'm',
-  'n',
-  'o',
-  'p',
-  'q',
-  'r',
-  's',
-  't',
-  'u',
-  'v',
-  'w',
-  'x',
-  'y',
-  'z'
-];
+var lowerCasedCharacters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 // Array of uppercase characters to be included in password
-var upperCasedCharacters = [
-  'A',
-  'B',
-  'C',
-  'D',
-  'E',
-  'F',
-  'G',
-  'H',
-  'I',
-  'J',
-  'K',
-  'L',
-  'M',
-  'N',
-  'O',
-  'P',
-  'Q',
-  'R',
-  'S',
-  'T',
-  'U',
-  'V',
-  'W',
-  'X',
-  'Y',
-  'Z'
-];
+var upperCasedCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
 //Global Variables
-let user = {
-              length: 8,
-              lowercase: false,
-              uppercase: false,
-              numeric: false,
-              special: false,
-}
+let user = { length: 8, lowercase: false, uppercase: false, numeric: false, special: false,};
 
 let userLength = 0;
 const form = document.getElementById('card-form');
@@ -104,12 +20,7 @@ const numberAlert = document.getElementById('number-alert');
 const characterAlert = document.getElementById('character-alert');
 
 let pass = true;
-const characterArray = [
-                      lowerCasedCharacters,
-                      upperCasedCharacters,
-                      numericCharacters, 
-                      specialCharacters
-                    ];
+const characterArray = [lowerCasedCharacters, upperCasedCharacters, numericCharacters, specialCharacters];
 let passArray;
 let selectedCharacterTypes = [];
 let randomPassword="";
@@ -117,14 +28,10 @@ let lowercaseTest = /[a-z]+/;
 let uppercaseTest = /[A-Z]+/;
 let numberTest = /[0-9]+/;
 let specialTest = /\W/;
-const testArray = [
-                    lowercaseTest, 
-                    uppercaseTest, 
-                    numberTest, 
-                    specialTest
-                  ];
+const testArray = [lowercaseTest, uppercaseTest, numberTest, specialTest];
 let boolArray = [];
 
+// Function to set initial values
 function initialise (){
   randomPassword = "";
   selectedCharacterTypes = [];
@@ -136,33 +43,34 @@ function initialise (){
 
 // Function to prompt user for password options
 function getPasswordOptions(object) {
-   // * Present a series of prompts for password criteria
+  // * Present a series of prompts for password criteria
   //   * Length of password
-userLength = parseInt(document.getElementById('form-number').value);
+  userLength = parseInt(document.getElementById('form-number').value);
 
   //     * At least 8 characters but no more than 128.
-    switch (userLength >= 8 && userLength <= 128){
-      case true:
-        numberAlert.style.display = 'none';
-        object.length = userLength;
-        characterTypes(user);
-        break;
-      default:
-        numberAlert.style.display = 'inline';
+  switch (userLength >= 8 && userLength <= 128){
+    case true:
+      numberAlert.style.display = 'none';
+      object.length = userLength;
+      characterTypes(user);
       break;
-    };
+    default:
+      numberAlert.style.display = 'inline';
+      break;
+  };
 };
 
+// Function for selecting the character types
 function characterTypes(object){
   //   * Character types
   //     * Lowercase
-    object.lowercase = document.getElementById('value1').checked;
+  object.lowercase = document.getElementById('value1').checked;
   //     * Uppercase
-    object.uppercase = document.getElementById('value2').checked;
+  object.uppercase = document.getElementById('value2').checked;
   //     * Numeric
-    object.numeric = document.getElementById('value3').checked;
+  object.numeric = document.getElementById('value3').checked;
   //     * Special characters ($@%&*, etc)
-    object.special = document.getElementById('value4').checked;
+  object.special = document.getElementById('value4').checked;
   
    // * Code should validate for each input and at least one character type should be selected
   passArray = Object.values(user);
@@ -170,61 +78,60 @@ function characterTypes(object){
 
   switch (passArray.includes(true)){
     case true:
-        characterAlert.style.display = 'none';
-      
-        for (let i=0; i < passArray.length; i++) {
-          if (passArray[i]){
-              selectedCharacterTypes = selectedCharacterTypes.concat(characterArray[i]);
-          };
+      characterAlert.style.display = 'none';
+    
+      for (let i=0; i < passArray.length; i++) {
+        if (passArray[i]){
+            selectedCharacterTypes = selectedCharacterTypes.concat(characterArray[i]);
         };
-        getRandom(selectedCharacterTypes);
-        form.style.display = 'none';
-        card.style.display = 'block';    
-        break;
+      };
+      getRandom(selectedCharacterTypes);
+      form.style.display = 'none';
+      card.style.display = 'block';    
+      break;
     default:
-        characterAlert.style.display = 'inline';
-        break;
+      characterAlert.style.display = 'inline';
+      break;
   };
 };
 
 // Function for getting a random element from an array
 function getRandom(arr) {
-
   // * Once prompts are answered then the password should be generated and displayed in an alert or written to the page
-for (let i=0; i<user.length; i++){
-  let ranNum = Math.floor(Math.random()*arr.length);
-  randomPassword += arr[ranNum];
-};
+  for (let i=0; i<user.length; i++){
+    let ranNum = Math.floor(Math.random()*arr.length);
+    randomPassword += arr[ranNum];
+  };
 
 //Check password contains all character types from user criteria
-for (i=0; i<passArray.length; i++) {
-  if(passArray[i] && testArray[i].test(randomPassword)) {
-    boolArray.push(true);
-  } else if (passArray[i] === false && testArray[i].test(randomPassword) === false) {
-    boolArray.push(true);
-  } else {
-    boolArray.push(false);
-  }
-};
+  for (i=0; i<passArray.length; i++) {
+    if (passArray[i] && testArray[i].test(randomPassword)) {
+      boolArray.push(true);
+    } else if (passArray[i] === false && testArray[i].test(randomPassword) === false) {
+      boolArray.push(true);
+    } else {
+      boolArray.push(false);
+    };
+  };
 
-if (boolArray.includes(false)) {
-  boolArray = [];
-  randomPassword = "";
-  getRandom(selectedCharacterTypes);
-} 
-pass = false;
-}
+  if (boolArray.includes(false)) {
+    boolArray = [];
+    randomPassword = "";
+    getRandom(selectedCharacterTypes);
+  }; 
+  pass = false;
+};
 
 // Function to generate password with user input
 function generatePassword() {
  // * Generate a password when the button is clicked
- if (pass){
-  getPasswordOptions(user);
-  return randomPassword;
- } else {
- initialise();
- }
-}
+  if (pass){
+    getPasswordOptions(user);
+    return randomPassword;
+  } else {
+    initialise();
+  };
+};
 
 // Get references to the #generate element
 var generateBtn = document.querySelector('#generate');
@@ -235,7 +142,7 @@ function writePassword() {
   var passwordText = document.querySelector('#password');
 
   passwordText.value = password;
-}
+};
 
 // Add event listener to generate button
 generateBtn.addEventListener('click', writePassword);
